f4a38bee2f071fc507096e2aef5e4fdd7e3edf9c
yancl <kmoving@gmail.com>
2013-04-23 11:44:21 +0800

    tune the struct

--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(
         description="python thrift client for cassandra",
         maintainer="yancl",
         maintainer_email="kmoving@gmail.com",
-        packages=['cassandra_client']
+        packages=[  'cassandra_client', 'cassandra_client.protocol',
+                    'cassandra_client.protocol.genpy',
+                    'cassandra_client.protocol.genpy.cassandra']
 )
