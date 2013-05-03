291696b14eac217fb531e7c6ddbe8594c6162421
yancl <kmoving@gmail.com>
2013-05-03 12:03:50 +0800

    add publish for setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python
+import os
+import sys
+from setuptools import setup
 
-from distutils.core import setup
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload -r internal')
+    sys.exit()
+
 
 setup(
         name = "cassandra_client",
         version = "0.10",
         description="python thrift client for cassandra",
-        maintainer="yancl",
-        maintainer_email="kmoving@gmail.com",
+        long_description=open("README.md").read(),
+        author="yancl",
+        author_email="kmoving@gmail.com",
+        url='https://github.com/yancl/cassandra_client',
+        classifiers=[
+            'Programming Language :: Python',
+        ],
+        platforms='Linux',
+        license='MIT License',
+        zip_safe=False,
+        install_requires=[
+            'thrift_client',
+        ],
+        tests_require=[
+            'nose',
+        ],
         packages=[  'cassandra_client', 'cassandra_client.protocol',
                     'cassandra_client.protocol.genpy',
                     'cassandra_client.protocol.genpy.cassandra']
